2bdbde48fc169168942bc4e7d9143772d8b0c050
Aleksey Alekseev <a.alekseev5743@yandex.ru>
2021-12-14 11:29:48 +0300

    add FilmManager, add tests

--- src/main/java/ru/netology/manager/FilmManager.java
+++ src/main/java/ru/netology/manager/FilmManager.java
@@ -1,4 +1,44 @@
 package ru.netology.manager;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.netology.domain.Film;
+
+@Data
+@NoArgsConstructor
 public class FilmManager {
+
+    private Film[] films = new Film[0];
+
+    public FilmManager(int resultLengthFilms) {
+        this.resultLengthFilms = resultLengthFilms;
+    }
+
+    private int resultLengthFilms = 10;
+
+    public void addFilm(Film film) {
+
+        int length = films.length + 1;
+        Film[] tmp = new Film[length];
+
+        System.arraycopy(films, 0, tmp, 0, films.length);
+
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = film;
+        films = tmp;
+    }
+
+    public Film[] getAll() {
+        if (films.length != 10 && films.length <= 10) {
+            resultLengthFilms = films.length;
+        } else {
+            resultLengthFilms = 10;
+        }
+        Film[] result = new Film[resultLengthFilms];
+        for (int i = 0; i < result.length; i++) {
+            int index = films.length - i - 1;
+            result[i] = films[index];
+        }
+        return result;
+    }
 }
